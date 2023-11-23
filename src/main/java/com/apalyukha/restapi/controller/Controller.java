ddf445c3575b270da9b6c7f5ad7028f15583e2cf
apalyukha <apalyukha@gmail.com>
2023-11-23 15:06:36 +0200

    addition of description swagger

--- src/main/java/com/apalyukha/restapi/controller/Controller.java
+++ src/main/java/com/apalyukha/restapi/controller/Controller.java
@@ -21,9 +21,9 @@ public class Controller {
     private final CatRepo catRepo;
 
     @Operation(
-            summary = "Додає нового котика у базу",
-            description = "Получає DTO котика і білдером " +
-                    "збирає і зберігає сутність у базу"
+            summary = "Adds a new cat to the database",
+            description = "Gets DTO cat and builder " +
+                    "collects and stores the essence in the database"
     )
     @PostMapping("/api/add")
     public void addCat(@RequestBody CatDTO catDTO) {
@@ -37,22 +37,34 @@ public class Controller {
         );
     }
 
+    @Operation(
+            summary = "List of all cats"
+    )
     @SneakyThrows
     @GetMapping("/api/all")
     public List<Cat> getAll() {
         return catRepo.findAll();
     }
 
+    @Operation(
+            summary = "Search by ID"
+    )
     @GetMapping("/api")
     public Cat getCat(@RequestParam int id) {
         return catRepo.findById(id).orElseThrow();
     }
 
+    @Operation(
+            summary = "Removing the cat from the database"
+    )
     @DeleteMapping("/api")
     public void delCat(@RequestParam int id) {
         catRepo.deleteById(id);
     }
 
+    @Operation(
+            summary = "Changing the cat's characteristics"
+    )
     @PutMapping("/api/add")
     public String changeCat(@RequestBody Cat cat) {
         if (!catRepo.existsById(cat.getId())) {
